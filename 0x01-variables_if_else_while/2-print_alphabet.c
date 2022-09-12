3d321093114cbee018e88bdb1b9c2f6cccf897f8
partary <jpartary@gmail.com>
2022-09-12 13:53:29 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint
+ *
+ * Return: ALways 0 (success)
+ *
+ */
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
+
