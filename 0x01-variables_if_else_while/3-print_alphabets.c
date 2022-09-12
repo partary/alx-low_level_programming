7975d4d3636dc1b182e5368486a5430d6c808018
partary <jpartary@gmail.com>
2022-09-12 13:58:27 -0700

    task 4 correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - print numbers from 0 to 9
+ * main - print alphabet in lower and uppercase
  *
- * Return: Always 0 (success)
  *
+ * Return: (0) Success
  */
 int main(void)
 {
-	int x;
+char ch;
 
-	for (x = 0; x < 10; x++)
-	{
-		printf("%d", x);
-	}
-	putchar('\n');
-	return (0);
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
