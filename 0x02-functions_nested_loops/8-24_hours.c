8fcddaac19c056064d5eb366e0f62931fa1d7e18
partary <jpartary@gmail.com>
2022-09-14 04:27:00 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - print every minute of the day
+ * jack_bauer - print every minute of the day 
  * starting from 00:00 to 23:59
  * @void: void
  * description: print every minute
@@ -13,16 +13,27 @@
 void jack_bauer(void)
 {
 	int a, b;
+
 	for (a = 0; a < 24; a++)
 	{
+
 		for (b = 0; b < 60; b++)
 		{
+
 			_putchar((a / 10) + '0');
+
 			_putchar((a % 10) + '0');
+
 			_putchar(':');
+
 			_putchar((b / 10) + '0');
+
 			_putchar((b % 10) + '0');
+
 			_putchar('\n');
+
 		}
+
 	}
+
 }
