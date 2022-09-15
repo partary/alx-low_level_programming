49981142b01b387e1fc7e8dd82c3d101a10ad8af
partary <jpartary@gmail.com>
2022-09-15 09:34:34 -0700

    TASK 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isupper - checks parameter is uppercase character.
+ * @c: input character.
+ * Return: 1 if uppercase character, 0 if not.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+	{
+
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+
+}
