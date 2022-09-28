9f891b19afc822c07d9a2e21d6285aa7c76924c7
partary <jpartary@gmail.com>
2022-09-28 03:08:08 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts_recursion - prints out a string
+ *
+ * @s:string to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
